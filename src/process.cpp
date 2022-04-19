7cee91f8d7245f9da03a2190d6a8218a5b14630c
Oreoluwa Seweje <67057368+Oreoluwa-Se@users.noreply.github.com>
2022-04-19 10:43:30 -0400

    Update process.cpp
    updated LinuxParser::ActiveJiffies and LinuxParser::StartTime arguments updated.

--- src/process.cpp
+++ src/process.cpp
@@ -25,8 +25,8 @@ int Process::Pid() { return pid_; }
 // DONE: Return this process's CPU utilization
 // https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat/16736599#16736599
 float Process::CpuUtilization() {
-  long c_active = LinuxParser::ActiveJiffies(Pid());
-  long c_total = LinuxParser::UpTime() - LinuxParser::StartTime(Pid());
+  long c_active = LinuxParser::ActiveJiffies(pid_);
+  long c_total = LinuxParser::UpTime() - LinuxParser::StartTime(pid_);
   auto out = 100 * c_active / c_total;
   return (float) out;
 }
